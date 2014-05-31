e4fc1d1dc8b90f503bd30aa784ef553612bd210e
Naveen Sharma <mss.naveensharma@gmail.com>
2014-05-31 10:45:32 +0530

    asasas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Servicetrac::Application.config.secret_key_base = 'dc08cb65105f404db80e2736b0114237a72f9669b9c98d58962981165ccc565ea890a4ed6aa1aed48e0427e0d1acd671ffcbf2a2e34e8511bac3c8eaa67f80a0'
