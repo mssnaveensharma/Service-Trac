b5bc98859e7812b525effbdf85aca2e798fcaa38
Naveen Sharma <mss.naveensharma@gmail.com>
2014-06-03 10:51:12 +0530

    sasas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602154254) do
+ActiveRecord::Schema.define(version: 20140602160223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20140602154254) do
     t.string   "Email"
     t.string   "Url"
     t.string   "ContactPerson"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "admin_tech_supports", force: true do |t|
+    t.string   "SupportDescription"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
