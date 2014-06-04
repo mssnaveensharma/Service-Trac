81a79227b21db2271a6533f12749261132f79154
Naveen Sharma <mss.naveensharma@gmail.com>
2014-06-04 12:23:20 +0530

    sasas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,11 @@
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate
   def index
   end
 
-  def show
-  end
-
- 
-
-  def create
-  end
+  
 
   def register_app_user
     if(params[:email] and params[:password] and params[:FirstName] and params[:LastName] and params[:EobrNumber] and params[:eobr_make_id] and params[:eobr_model_id] and params[:TruckNumber] and params[:truckmake] and params[:TruckYear] and params[:TruckOwner] and params[:CompanyName] and params[:tech_support] and params[:Contact] )
@@ -30,7 +25,7 @@ class UsersController < ApplicationController
           :company_id=>params[:CompanyName],
           :tech_support_id=>params[:tech_support],
           :Language=>params[:Language],
-          :Role=>"app_user"
+          :Role=>"AppUser"
         });
 
       if(@response.id !='' and @response.id != nil)
@@ -61,5 +56,10 @@ end
     def user_params
       #params.require(:user).permit!
       params.require(:user).permit(:eobr_make_id,:eobr_model_id,:EobrNumber,:TruckNumber,:truckmake,:TruckYear,:TruckOwner,:CompanyName,:FirstName,:LastName,:email,:encrypted_password,:password_confirmation,:Language)
+    end
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        token == TOKEN
+      end
     end
 end
