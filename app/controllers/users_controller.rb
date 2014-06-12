62b61150fbb3609564224b66c64f44b138c0c416
Naveen Sharma <mss.naveensharma@gmail.com>
2014-06-12 18:01:52 +0530

    sas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+   skip_before_action :verify_authenticity_token
   
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate
