c18dda14c7e657cc34a258a3bffffad89cfaacbc
mss_randhir <mss.randhir@gmail.com>
2014-06-19 11:51:41 +0530

    added contact to tech support

--- app/models/admin/tech_support.rb
+++ app/models/admin/tech_support.rb
@@ -1,4 +1,5 @@
 class Admin::TechSupport < ActiveRecord::Base
   has_many :users
   validates :SupportDescription, presence: true,length: { maximum: 25 }
+  validates :Contact, presence: true,length: { maximum: 15 },numericality: { only_integer: true, message: "is not a valid number" }
 end
