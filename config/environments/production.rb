d33875ce503bd284182186172c409b9c4c2d9305
Naveen Sharma <mss.naveensharma@gmail.com>
2014-06-25 11:37:30 +0530

    sas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Servicetrac::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   #url to heroku instance.
-  config.action_mailer.default_url_options = { :host => 'servicetrac.herokuapp.com' , :protocol => 'http'}
+  config.action_mailer.default_url_options = { :host => 'myservicetrac.com' , :protocol => 'http'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
